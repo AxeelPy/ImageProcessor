01902458880c8ba11b4fd4a6e9d1a49f7214e9ab
AxeelPy <aheinner11@gmail.com>
2024-03-06 15:27:17 -0300

    first algorythm

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import PIL
-from PIL import Image, ImageStat
+from PIL import Image, ImageStat, ImageEnhance
+import multiprocessing as mp
 
-im = PIL.Image.open("images/light.jpg")
+im = PIL.Image.open("images/rndm2.jpg")
 bw = im.convert("L")
+imagebr = ImageStat.Stat(bw)
+imagebr = imagebr.rms
 
-bw.show()
+print("Dark image RMS:", imagebr, "\nFrom 1: " + str(float(imagebr[0]) / 100))
+Enhanceamount = 2 - float(imagebr[0]) / 100
+enhanced = ImageEnhance.Brightness(im).enhance(Enhanceamount)
+enhancedbw = enhanced.convert("L")
+enhancedbw = ImageStat.Stat(enhancedbw).rms
+print(f"Enhanced from {str(float(imagebr[0]) / 100)} to {Enhanceamount}")
+print(f"Current brightness is {enhancedbw}")
+
+lastenhance = str(float(imagebr[0]))
+i = 2
+while enhancedbw[0] < 100:
+    Enhanceamount = i - float(enhancedbw[0]) / 100
+    print(Enhanceamount)
+    enhanced = ImageEnhance.Brightness(im).enhance(Enhanceamount)
+    enhancedbw = enhanced.convert("L")
+    enhancedbw = ImageStat.Stat(enhancedbw).rms
+    print(f"Enhanced from {lastenhance} to {Enhanceamount}")
+    print(f"Current brightness is {enhancedbw}")
+    lastenhance = enhancedbw
+    i = i + .5
+
+enhanced.show()
+enhanced.save("images/newimg.jpg")
