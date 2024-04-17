6637cbade66b23c96c2e353d4cc22157ef98e8fa
AxeelPy <aheinner11@gmail.com>
2024-04-17 15:49:41 -0400

    Update?

--- img_module_2.py
+++ img_module_2.py
@@ -7,7 +7,6 @@ import random
 def imgprocessor(file: str, fixedbrightness: float=100, precision: float=0.5):
     if file == "": return {"error": True, "reason": "File path not given"}  # If error is True, there has to be a reason
     if fixedbrightness == "": fixedbrightness = 100
-    if fixedbrightness > 250: fixedbrightness = 200
     if precision == "": precision = 0.5
     if not os.path.exists(file): return {"error": True, "reason": "File does not exist"}
 
@@ -20,34 +19,12 @@ def imgprocessor(file: str, fixedbrightness: float=100, precision: float=0.5):
     except Exception:
         return {"error": True, "reason": "Probably not an image"}
 
-    width, height = img.size[0], img.size[1]
+    img_bw = img.convert("L")
+    img_rms = ImageStat.Stat(img_bw).rms
 
-    left_top = img.crop((0, 0, width//5, height//5))
-    right_top = img.crop((width-width//5, 0, width, height//5))
-
-    left_bottom = img.crop((0, height-height//5, width//5, height))
-    right_bottom = img.crop((width-width//5, height-height//5, width, height))
-
-    left_top_bw = left_top.convert("L")
-    left_top_bw = ImageStat.Stat(left_top_bw).rms
-
-    right_top_bw = right_top.convert("L")
-    right_top_bw = ImageStat.Stat(right_top_bw).rms
-
-    left_bottom_bw = left_bottom.convert("L")
-    left_bottom_bw = ImageStat.Stat(left_bottom_bw).rms
-
-    right_bottom_bw = right_bottom.convert("L")
-    right_bottom_bw = ImageStat.Stat(right_bottom_bw).rms
-
-    rmslist = [left_top_bw[0], left_bottom_bw[0], right_bottom_bw[0], right_top_bw[0]]
-    print(rmslist)
-
-    total = float(rmslist[0]) + float(rmslist[1]) + float(rmslist[2]) + float(rmslist[3])
-    print(f"Total: {total}")
-    average = total / len(rmslist)
-    print(f"Average: {average}")
-    enhanced = ImageEnhance.Brightness(img).enhance(100/average)
+    print(f"RMS: {img_rms}")
+    enhanced = ImageEnhance.Brightness(img).enhance(fixedbrightness/float(img_rms[0]))
+    print(f"Target brightness is {fixedbrightness}")
 
     while True:
         save = str(random.randint(100000, 999999))
